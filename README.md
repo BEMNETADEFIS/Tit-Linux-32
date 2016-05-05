efc6ee44792bdb18204986573601b89f390de16b
Sandeep Patil <sandeep@exploreembedded.com>
2016-05-05 16:14:17 +0530

    support for linux 32 bit machines
    The tool chain has not been tested, needs to be tested on a 32 bit
    machine.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tit-Linux-32
+Arduino tool chain support for Tit on Linux-32. Tit is Arduino SW compatible Atmega328PB based board in Arduino pro-mini form factor.
+
+To install support for Tit on Linux-32, simply extract the contents the latest release to **/hardware** directory of your arduino installation
+
+After extracting the directory structure should look like
+**..\Documents\Arduino\hardware\Tit-Linux-32**
+
+To find out the location, click  File>Preferences and look for 'Sketchbook location'. Usually it is under documents/arduino/hardware. If you do not find a directory named hardware, create it. 
+
+
+![Tit](http://exploreembedded.com/wiki/images/a/a5/Tit-2.JPG)
+
+##References and Credits:
+Thanks to work by Andre Moehl on *uino Mini Super (Atmega328PB) project on hackaday.io
+https://hackaday.io/project/9313-uino-mini-super-atmega328pb
---
+++ Tit-Linux-32/tools/lib/gcc/avr/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/toolsbuild/workspace/avr8-gnu-toolchain/src/gcc/configure LDFLAGS=-L/home/toolsbuild/workspace/avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86-hostlibs/lib CPPFLAGS= --target=avr --host=i686-pc-linux-gnu --build=x86_64-pc-linux-gnu --prefix=/home/toolsbuild/workspace/avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86 --libdir=/home/toolsbuild/workspace/avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86/lib --enable-languages=c,c++ --with-dwarf2 --enable-doc --disable-shared --disable-libada --disable-libssp --disable-nls --with-avrlibc=yes --with-mpfr=/home/toolsbuild/workspace/avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86-hostlibs --with-gmp=/home/toolsbuild/workspace/avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86-hostlibs --with-mpc=/home/toolsbuild/workspace/avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86-hostlibs --with-pkgversion=AVR_8_bit_GNU_Toolchain_3.5.1_1671 --with-bugurl=http://www.atmel.com";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
